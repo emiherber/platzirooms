534deba57deee37a2eaf1ff2e1ff2c342242f08a
emiherber <emiherbes@hotmail.com>
2019-10-27 19:32:04 -0300

    Commit Init

---
+++ src/main.js
@@ -0,0 +1,36 @@
+import Vue from 'vue';
+import firebase from 'firebase';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    store.dispatch('FETCH_AUTH_USER');
+  }
+});
+
+new Vue({
+  router,
+  store,
+  render: h => h(App),
+  beforeCreate() {
+    if (store.state.authId) {
+      this.$store.dispatch('FETCH_USER', { id: store.state.authId });
+    }
+  },
+}).$mount('#app');
